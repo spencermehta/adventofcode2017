f90bd5554b929fa1d921b1661be24a1807db822e
Spencer Mehta <spencermehta@gmail.com>
2022-06-03 18:15:31 +0100

    feat: day04 part a improvements and day04 part b

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -20,35 +20,68 @@ pub fn main() -> io::Result<()> {
     // println!("{:?}", nums);
 
     let ans_a = part_a(&nums);
-    // let ans_b = part_b(&nums);
+    let ans_b = part_b(&nums);
 
-    println!("{}\n{}", ans_a, false);
+    println!("{}\n{}", ans_a, ans_b);
 
     Ok(())
 }
 
-fn part_a(lines: &Vec<Vec<String>>) -> u32 {
+fn part_a(lines: &[Vec<String>]) -> u32 {
     lines
         .iter()
         .map(valid_passphrase)
         .fold(0, |acc, e| if e { acc + 1 } else { acc })
 }
 
+fn part_b(lines: &[Vec<String>]) -> u32 {
+    lines
+        .iter()
+        .map(valid_passphrase_anagrams)
+        .fold(0, |acc, e| if e { acc + 1 } else { acc })
+}
+
 fn valid_passphrase(phrase: &Vec<String>) -> bool {
-    let mut p = phrase.clone(); // this seems sketchy lol
+    let mut p = phrase.clone();
     p.sort();
-    let mut q = p.clone();
-    q.pop();
-    !q.iter()
+    !p.iter()
         .enumerate()
-        .map(|(index, word)| word == &p[index + 1])
+        .map(|(index, word)| {
+            if index == p.len() - 1 {
+                return false;
+            }
+            word == &p[index + 1]
+        })
+        .reduce(|acc, e| acc || e)
+        .unwrap()
+}
+
+fn valid_passphrase_anagrams(phrase: &Vec<String>) -> bool {
+    let mut p = phrase.clone();
+    p = p
+        .iter()
+        .map(|word| {
+            let mut chars: Vec<char> = word[..].chars().collect();
+            chars.sort();
+            chars.iter().collect()
+        })
+        .collect();
+    p.sort();
+    !p.iter()
+        .enumerate()
+        .map(|(index, word)| {
+            if index == p.len() - 1 {
+                return false;
+            }
+            word == &p[index + 1]
+        })
         .reduce(|acc, e| acc || e)
         .unwrap()
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::valid_passphrase;
+    use crate::{valid_passphrase, valid_passphrase_anagrams};
 
     #[test]
     fn no_duplicates() {
@@ -97,6 +130,56 @@ mod tests {
             String::from("cc"),
             String::from("dd"),
             String::from("aaa"),
+        ]))
+    }
+
+    #[test]
+    fn case_b_one() {
+        assert!(valid_passphrase_anagrams(&vec![
+            String::from("abcde"),
+            String::from("fghij"),
+        ]))
+    }
+
+    #[test]
+    fn case_b_two() {
+        assert!(!valid_passphrase_anagrams(&vec![
+            String::from("abcde"),
+            String::from("xyz"),
+            String::from("ecdab"),
+        ]))
+    }
+
+    #[test]
+    fn case_b_three() {
+        assert!(valid_passphrase_anagrams(&vec![
+            String::from("a"),
+            String::from("ab"),
+            String::from("abc"),
+            String::from("abd"),
+            String::from("abf"),
+            String::from("abj"),
+        ]))
+    }
+
+    #[test]
+    fn case_b_four() {
+        assert!(valid_passphrase_anagrams(&vec![
+            String::from("iiii"),
+            String::from("oiii"),
+            String::from("ooii"),
+            String::from("oooi"),
+            String::from("oooo"),
+        ]))
+    }
+
+    #[test]
+    fn case_b_five() {
+        assert!(!valid_passphrase_anagrams(&vec![
+            String::from("oiii"),
+            String::from("ioii"),
+            String::from("iioi"),
+            String::from("iiio"),
         ]))
     }
 }
